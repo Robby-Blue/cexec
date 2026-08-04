d1050e7df301b33194ed05b3170268d28656627c
Robby-Blue <69482496+Robby-Blue@users.noreply.github.com>
2026-08-04 23:18:02 +0200

    add auto exit option

--- client/src/main.py
+++ client/src/main.py
@@ -5,6 +5,11 @@ import api
 import json
 import time
 
+with open("config.json") as f:
+    config = json.load(f)
+
+auto_exit = config.get("auto_exit", True)
+
 def main():
     with open("version.json") as f:
         version_id = json.load(f)["version_id"]
@@ -14,8 +19,11 @@ def main():
         task = get_next_task()
         
         if not task["found"]:
-            time.sleep(60 * 60)
-            continue
+            if auto_exit:
+                return
+            else:
+                time.sleep(60 * 60)
+                continue
         
         task_runner.run_task(task)
 
